361f31557265e069d348da61b9573529283551ca
Victor Merseguel <merseguel@gmail.com>
2024-02-03 15:04:59 +0000

    fix footer margin

--- src/pages/TermsOfUse.jsx
+++ src/pages/TermsOfUse.jsx
@@ -1,7 +1,11 @@
 import styles from "./TermsOfUse.module.css";
 import { terms_db } from "../databases/terms_db";
+import { useEffect } from "react";
 
 const TermsOfUse = ({ lang }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className={styles.terms_wrap}>
       <h3>{terms_db[0][lang]}</h3>
